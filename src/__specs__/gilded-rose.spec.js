722f9b13524e481c93e4be87dfe758038142e373
netlocolerk16@hotmail.com <netlocolerk16@hotmail.com>
2020-08-07 14:51:16 -0500

    working test

--- src/__specs__/gilded-rose.spec.js
+++ src/__specs__/gilded-rose.spec.js
@@ -22,7 +22,8 @@ describe("GildedRose", () => {
       new Item("Sulfuras, Hand of Ragnaros", 0, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
     ];
-    const gildedRose = new GildedRose(storeItems);
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
     gildedRose.updateQuality();
 
     expect(gildedRose.items).to.eql(expectedResult);
@@ -31,44 +32,82 @@ describe("GildedRose", () => {
   it("for normal items quality should never be below 0", () => {
     const storeItems = [new Item("+5 Dexterity Vest", 10, 0)];
     const expectedResult = [new Item("+5 Dexterity Vest", 9, 0)];
-    const gildedRose = new GildedRose(storeItems);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
 
-    expect(items).to.eql(expectedResult);
+    expect(gildedRose.items).to.eql(expectedResult);
   });
 
   it("when the sellIn date passes, quality should degrade twice as fast", () => {
     const storeItems = [new Item("+5 Dexterity Vest", 0, 4)];
     const expectedResult = [new Item("+5 Dexterity Vest", -1, 2)];
-    const gildedRose = new GildedRose(storeItems);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
 
-    expect(items).to.eql(expectedResult);
+    expect(gildedRose.items).to.eql(expectedResult);
   });
   it("the quality of an item can never be more than 50", () => {
     const storeItems = [new Item("Aged Brie", 1, 50)];
     const expectedResult = [new Item("Aged Brie", 0, 50)];
-    const gildedRose = new GildedRose(storeItems);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
 
-    expect(items).to.eql(expectedResult);
+    expect(gildedRose.items).to.eql(expectedResult);
   });
   it("the quality of an aged brie should increase by 1", () => {
     const storeItems = [new Item("Aged Brie", 1, 0)];
     const expectedResult = [new Item("Aged Brie", 0, 1)];
-    const gildedRose = new GildedRose(storeItems);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
 
-    expect(items).to.eql(expectedResult);
+    expect(gildedRose.items).to.eql(expectedResult);
   });
 
   it("the quality of conjured items should decrease twice as fast", () => {
     const storeItems = [new Item("Conjured Mana Cake", 10, 20)];
     const expectedResult = [new Item("Conjured Mana Cake", 9, 18)];
-    const gildedRose = new GildedRose(storeItems);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
 
-    expect(items).to.eql(expectedResult);
+    expect(gildedRose.items).to.eql(expectedResult);
+  });
+
+  it("quality of the item is never negative", () => {
+    const storeItems = [new Item("Conjured Mana Cake", 0, 0)];
+    const expectedResult = [new Item("Conjured Mana Cake", -1, 0)];
+
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
+
+    expect(gildedRose.items).to.eql(expectedResult);
+  });
+
+  it("Sulfuras, being a legendary item, never has to be sold or decreases in quality", () => {
+    const storeItems = [new Item("Sulfuras", 0, 80)];
+    const expectedResult = [new Item("Sulfuras", -1, 80)];
+
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
+
+    expect(gildedRose.items).to.eql(expectedResult);
+  });
+
+  it("aged brie quailty increases as it gets older", () => {
+    const storeItems = [new Item("Aged Brie", 0, 0)];
+    const expectedResult = [new Item("Aged Brie", -1, 1)];
+
+    const gildedRose = new GildedRose();
+    gildedRose.items = storeItems;
+    gildedRose.updateQuality();
+
+    expect(gildedRose.items).to.eql(expectedResult);
   });
 
   describe("Backstage passes", () => {
@@ -79,10 +118,11 @@ describe("GildedRose", () => {
       const expectedResult = [
         new Item("Backstage passes to a TAFKAL80ETC concert", 13, 1),
       ];
-      const gildedRose = new GildedRose(storeItems);
-      const items = gildedRose.updateQuality();
+      const gildedRose = new GildedRose();
+      gildedRose.items = storeItems;
+      gildedRose.updateQuality();
 
-      expect(items).to.eql(expectedResult);
+      expect(gildedRose.items).to.eql(expectedResult);
     });
 
     it("Quality increases by 2 when there are 10 days or less", () => {
@@ -92,10 +132,11 @@ describe("GildedRose", () => {
       const expectedResult = [
         new Item("Backstage passes to a TAFKAL80ETC concert", 9, 2),
       ];
-      const gildedRose = new GildedRose(storeItems);
-      const items = gildedRose.updateQuality();
+      const gildedRose = new GildedRose();
+      gildedRose.items = storeItems;
+      gildedRose.updateQuality();
 
-      expect(items).to.eql(expectedResult);
+      expect(gildedRose.items).to.eql(expectedResult);
     });
 
     it("Quality increases by 3 when there are 5 days or less", () => {
@@ -105,10 +146,11 @@ describe("GildedRose", () => {
       const expectedResult = [
         new Item("Backstage passes to a TAFKAL80ETC concert", 4, 3),
       ];
-      const gildedRose = new GildedRose(storeItems);
-      const items = gildedRose.updateQuality();
+      const gildedRose = new GildedRose();
+      gildedRose.items = storeItems;
+      gildedRose.updateQuality();
 
-      expect(items).to.eql(expectedResult);
+      expect(gildedRose.items).to.eql(expectedResult);
     });
 
     it("Quality drops to 0 after concert", () => {
@@ -118,10 +160,11 @@ describe("GildedRose", () => {
       const expectedResult = [
         new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
       ];
-      const gildedRose = new GildedRose(storeItems);
-      const items = gildedRose.updateQuality();
+      const gildedRose = new GildedRose();
+      gildedRose.items = storeItems;
+      gildedRose.updateQuality();
 
-      expect(items).to.eql(expectedResult);
+      expect(gildedRose.items).to.eql(expectedResult);
     });
   });
 });
